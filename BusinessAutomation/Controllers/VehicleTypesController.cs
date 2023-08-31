2dfe5dd28792514d3fc9a362f0b2bd8ba249e019
Anold Kupara <kuparaanold20@gmail.com>
2023-08-31 12:07:00 +0200

    added the VehicleTypesController

---
+++ BusinessAutomation/Controllers/VehicleTypesController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BusinessAutomation.Data;
+using BusinessAutomation.Domain.Finance.VehicleEntity;
+
+namespace BusinessAutomation.Controllers
+{
+    public class VehicleTypesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public VehicleTypesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: VehicleTypes
+        public async Task<IActionResult> Index()
+        {
+              return _context.VehicleTypes != null ? 
+                          View(await _context.VehicleTypes.ToListAsync()) :
+                          Problem("Entity set 'ApplicationDbContext.VehicleTypes'  is null.");
+        }
+
+        // GET: VehicleTypes/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null || _context.VehicleTypes == null)
+            {
+                return NotFound();
+            }
+
+            var vehicleType = await _context.VehicleTypes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (vehicleType == null)
+            {
+                return NotFound();
+            }
+
+            return View(vehicleType);
+        }
+
+        // GET: VehicleTypes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: VehicleTypes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Description")] VehicleType vehicleType)
+        {
+            if (ModelState.IsValid)
+            {
+                vehicleType.Id = Guid.NewGuid();
+                _context.Add(vehicleType);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(vehicleType);
+        }
+
+        // GET: VehicleTypes/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null || _context.VehicleTypes == null)
+            {
+                return NotFound();
+            }
+
+            var vehicleType = await _context.VehicleTypes.FindAsync(id);
+            if (vehicleType == null)
+            {
+                return NotFound();
+            }
+            return View(vehicleType);
+        }
+
+        // POST: VehicleTypes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Id,Name,Description")] VehicleType vehicleType)
+        {
+            if (id != vehicleType.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(vehicleType);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!VehicleTypeExists(vehicleType.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(vehicleType);
+        }
+
+        // GET: VehicleTypes/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null || _context.VehicleTypes == null)
+            {
+                return NotFound();
+            }
+
+            var vehicleType = await _context.VehicleTypes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (vehicleType == null)
+            {
+                return NotFound();
+            }
+
+            return View(vehicleType);
+        }
+
+        // POST: VehicleTypes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            if (_context.VehicleTypes == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.VehicleTypes'  is null.");
+            }
+            var vehicleType = await _context.VehicleTypes.FindAsync(id);
+            if (vehicleType != null)
+            {
+                _context.VehicleTypes.Remove(vehicleType);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool VehicleTypeExists(Guid id)
+        {
+          return (_context.VehicleTypes?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
