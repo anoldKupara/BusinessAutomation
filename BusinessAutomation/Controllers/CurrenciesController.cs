7286652e9fbd308d41005f877ab18a1d5092e26f
Anold Kupara <kuparaanold20@gmail.com>
2023-08-31 11:43:50 +0200

    added CurrenciesController and view

---
+++ BusinessAutomation/Controllers/CurrenciesController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BusinessAutomation.Data;
+using BusinessAutomation.Domain.Finance.Currencies;
+
+namespace BusinessAutomation.Controllers
+{
+    public class CurrenciesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CurrenciesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Currencies
+        public async Task<IActionResult> Index()
+        {
+              return _context.Currencies != null ? 
+                          View(await _context.Currencies.ToListAsync()) :
+                          Problem("Entity set 'ApplicationDbContext.Currencies'  is null.");
+        }
+
+        // GET: Currencies/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null || _context.Currencies == null)
+            {
+                return NotFound();
+            }
+
+            var currency = await _context.Currencies
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (currency == null)
+            {
+                return NotFound();
+            }
+
+            return View(currency);
+        }
+
+        // GET: Currencies/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Currencies/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,CurrencyCode")] Currency currency)
+        {
+            if (ModelState.IsValid)
+            {
+                currency.Id = Guid.NewGuid();
+                _context.Add(currency);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(currency);
+        }
+
+        // GET: Currencies/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null || _context.Currencies == null)
+            {
+                return NotFound();
+            }
+
+            var currency = await _context.Currencies.FindAsync(id);
+            if (currency == null)
+            {
+                return NotFound();
+            }
+            return View(currency);
+        }
+
+        // POST: Currencies/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Id,Name,CurrencyCode")] Currency currency)
+        {
+            if (id != currency.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(currency);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CurrencyExists(currency.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(currency);
+        }
+
+        // GET: Currencies/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null || _context.Currencies == null)
+            {
+                return NotFound();
+            }
+
+            var currency = await _context.Currencies
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (currency == null)
+            {
+                return NotFound();
+            }
+
+            return View(currency);
+        }
+
+        // POST: Currencies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            if (_context.Currencies == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Currencies'  is null.");
+            }
+            var currency = await _context.Currencies.FindAsync(id);
+            if (currency != null)
+            {
+                _context.Currencies.Remove(currency);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CurrencyExists(Guid id)
+        {
+          return (_context.Currencies?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
